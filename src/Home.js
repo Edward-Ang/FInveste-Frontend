d04272148edb4f43c23c856cc50be9ca276efea0
Edward Ang <wei.hen.ang@gmail.com>
2024-04-19 21:41:24 +0800

    make header component

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './css/home.css';
 import './css/main.css';
 import './css/header.css';
+import Header from './Header';
 
 function Home() {
-    const navigate = useNavigate();
-    const [showConfirmLogout, setShowConfirmLogout] = useState(false);
     const [stocks, setStocks] = useState([]);
 
     useEffect(() => {
@@ -24,64 +22,9 @@ function Home() {
         getTable();
     }, []); // The empty array means this effect runs once after the initial render
 
-    const handleLogout = () => {
-        setShowConfirmLogout(true); // Show the logout confirmation overlay
-    };
-
-    const handleCancelLogout = () => {
-        setShowConfirmLogout(false); // Hide the logout confirmation overlay
-    };
-
-    const handleConfirmLogout = () => {
-        navigate('/login');
-    };
-
     return (
         <>
-            <header>
-                <div className="headerbar">
-                    <div className="headerLogo">
-                        <img src="/images/6888.KL.svg" alt="logo" />
-                        <a href="/home">FInvesté</a>
-                    </div>
-                    <div className="searchbar" onClick={(event) => event.stopPropagation()}>
-                        <input type="text" autoComplete="off" id="filterInput" placeholder="Search" />
-                    </div>
-                    <div className="headerIcon" id="dropdownMenu">
-                        <button id="dropdownButton"><i className="fa-solid fa-bars"></i></button>
-                        <ul id="dropdownContent">
-                            <li id="watchlistButtonLi">
-                                <a id="watchlistButton" href="/watchlist">Watchlist</a>
-                            </li>
-                            <li>
-                                <button id="accountButton" onClick={handleLogout}>Logout</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </header>
-            {/* Logout confirmation overlay */}
-            {showConfirmLogout && (
-                <div className="confirmOverlay" id="logoutOverlay">
-                    <div className="confirmPopup">
-                        <div className="confirmWrapper">
-                            <div className="confirmHeader">
-                                <h5>Confirm to logout?</h5>
-                            </div>
-                            <div className="confirmBody">
-                                <div className="confirmButtons">
-                                    <button type="button" className="cancelButton" onClick={handleCancelLogout}>
-                                        Cancel
-                                    </button>
-                                    <button type="button" className="confirmButton" onClick={handleConfirmLogout}>
-                                        Confirm
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
+        <Header />
             <div className="bodyWrapper">
                 <div className="leftContainer">
                     <div className="card">
